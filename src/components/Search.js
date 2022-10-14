6e284a225e23bd4f3b73f6f461acaaaa05d6fcbc
Hammed Mujeeb <hamkamolujide@gmail.com>
2022-10-14 22:54:10 +0100

    update funcs

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,13 +1,21 @@
 import React from 'react'
+import { useState } from 'react'
 
-const Search = () => {
+const Search = ({search}) => {
+  const [searchInput, setsearchInput] = useState("")
+  const callSearchFunction = (e) => {
+    e.preventDefault();
+    search(searchInput);
+    setsearchInput("")
+  }
   return (
+    
     <>
 
         <div className="container-fluid search mt-3">
-            <input type="text" className='form-control' placeholder='Search your favourite movies' name="searchItem" id="" />
+            <input type="text" onChange={(e)=>{setsearchInput(e.target.value)}} className='form-control' placeholder='Search your favourite movies' name="searchItem" id="" />
             <div className="input-group-append">
-                <button className='btn btn-danger '>Search</button>
+                <button className='btn btn-danger' onClick={callSearchFunction}>Search</button>
             </div>
         </div>
     
