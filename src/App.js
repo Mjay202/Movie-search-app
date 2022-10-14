1d57cee06deb1bd6d85d67b0fdfafc752ca08b5e
Hammed Mujeeb <hamkamolujide@gmail.com>
2022-10-14 13:35:59 +0100

    fixed map func

--- src/App.js
+++ src/App.js
@@ -4,38 +4,64 @@ import NavBar from './components/NavBar';
 import Search from './components/Search';
 import {useState, useEffect} from "react";
 import Movies from './components/Movies';
+import img1 from "./404-error-pic.jpg"
+import axios from "axios"
 
 function App() {
 
-  const MOVIE_API_URL = "http://www.omdbapi.com/?apikey=a3ad1c44&s=vikings";
+  const endpoint = "http://www.omdbapi.com/?apikey=a3ad1c44&s=vikings"
   const [isLoading, setisLoading] = useState(true)
   const [movies, setmovies] = useState([])
   const [errormessage, seterrormessage] = useState("")
+  // const [poster, setposter] = useState("")
+  const [movies1, setmovies1] = useState([])
 
-
+  
   useEffect(() => {
     
-    fetch(MOVIE_API_URL)
-    .then(response => response.json())
-    .then(jsonResponse =>{
-        // setmovies(jsonResponse.Search)
-        setmovies(jsonResponse.Search)
-        setisLoading(false)
-    } )
-    
+    axios.get(endpoint)
+    .then(response=>{
+      setmovies(response.data.Search)
+      console.log(response.data.Search)
+      
+    })
+
+    // fetch(MOVIE_API_URL)
+    // .then(response => response.json())
+    // .then(jsonResponse =>{
+    //   movies = jsonResponse.Poster
+    //   setmovies1(jsonResponse.Search)
+    //   setisLoading(false)
+    //   console.log(movies)
+    //   console.log(movies1)
+      
+        // setposter(jsonResponse.Poster)
+        // setisLoading(false)
+    // } )
+
+
   }, [])
+ 
+
   
+ 
   return (
     <>
         <NavBar/>
         <Search />
-
-        {movies.map((movie, index)=>{
-          <Movies key={index} movie={movie}/>
-        })}
+        {/* <div className="container-fluid">
+          <div className="row">
         
-            
-       
+        </div>
+        </div> */}
+      <div className="container-fluid bg-transparent mt-4">
+        <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 ">  
+        {movies.map((movie, index)=>(
+           <Movies key={index} movie={movie}/>
+          
+        ))}
+        </div>
+      </div> 
     </>
   );
 }
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,17 +1,34 @@
 import React from 'react'
+import errorpic from "../404-boy.jpg"
 
-const Movies = (movie) => {
+  const Movies = ({movie}) => {
 
-    const poster = movie.Poster === "N/A"? 
+   
   return (
     <>
-        <div className="card-group bg-dark">
-            <div className="card">
-            <div className="card-title">{movie.Title}</div>
-            <img src="" className='img-fluid' alt="" />
-            </div>  
+       
+        {/* <div className="card">
+          <div className="card-title">
+            <h1>{movie.Title}</h1>
+          </div>
+          <img src={movie.Poster} alt={movie.Title} className="img-fluid"/>
+       </div> */}
+       <div className="col">
+          <div className="card bg-transparent border-danger shadow-box m-1" >
+            <div className="row no-gutters">
+              <div className="col-md-5">
+                <img src={movie.Poster} alt={movie.Title} className="img-fluid m-image"/>
+              </div>
+              <div className="col-md-7 bg-dark ">
+                <div className="card-body text-white">
+                  <h5 className="card-title">{movie.Title}</h5>
+                  <p className="card-text">{movie.Plot}</p>
+                  <p className="card-text"><small className="text-muted">{movie.Year}</small></p>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-    
     </>
   )
 }
